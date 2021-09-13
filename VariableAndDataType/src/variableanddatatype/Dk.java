e74483f41b8bf0206faa3ee9d2d92858adf9088c
fahimahammed <fahimahammed.cse@gmail.com>
2021-09-13 16:03:18 +0600

    dk java

---
+++ VariableAndDataType/src/variableanddatatype/Dk.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variableanddatatype;
+
+/**
+ *
+ * @author Fahim
+ */
+public class Dk {
+    public static void main(String[] args) {
+        int p = 10;
+        int num = 20;
+        num = p;
+        System.out.println(num);
+    }
+}
