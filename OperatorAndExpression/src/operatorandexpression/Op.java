e7119eec0fef2d984ae301078f9c32a6ad923ebe
fahimahammed <fahimahammed.cse@gmail.com>
2021-09-13 19:24:03 +0600

    operator and expression

---
+++ OperatorAndExpression/src/operatorandexpression/Op.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operatorandexpression;
+
+/**
+ *
+ * @author Fahim
+ */
+public class Op {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        System.out.println("Sum: "+(a+b));
+        System.out.println("Sub: "+(a-b));
+        System.out.println("Mul: "+(a*b));
+        System.out.println("Div: "+(a/b));
+        System.out.println("Mod: "+(a%b));
+        System.out.println("Decrement: "+(--a));
+        System.out.println("Increment: "+(++a));
+    }
+}
