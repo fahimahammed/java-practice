6b9710fc9057ed3578af35cfd812950539eda0d6
fahimahammed <fahimahammed.cse@gmail.com>
2021-09-13 16:05:38 +0600

    const java

---
+++ VariableAndDataType/src/variableanddatatype/Constant.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variableanddatatype;
+
+/**
+ *
+ * @author Fahim
+ */
+public class Constant {
+    public static void main(String[] args) {
+        final double pi = 3.14156;
+        System.out.println("const: "+pi);
+    }
+}
