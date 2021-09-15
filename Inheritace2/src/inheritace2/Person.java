126b7125f6c98828c96307afb5597f3e41090434
fahimahammed <fahimahammed.cse@gmail.com>
2021-09-15 19:04:49 +0600

    inheritance practice 2

---
+++ Inheritace2/src/inheritace2/Person.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritace2;
+
+/**
+ *
+ * @author Fahim
+ */
+public class Person {
+    String name = "Fahim";
+    int age;
+    int roll;
+    int id;
+}
---
+++ Inheritace2/src/inheritace2/Show.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritace2;
+
+/**
+ *
+ * @author Fahim
+ */
+public class Show extends Person{
+    void showPerson(){
+        System.out.println(name);
+    }
+}
