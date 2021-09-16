4a92fee83e1fcfc2730ee956e5b7b86d82a414ad
fahimahammed <fahimahammed.cse@gmail.com>
2021-09-16 20:40:11 +0600

    polymorphism

---
+++ Polymorphism/src/polymorphism/Polymorphism.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polymorphism;
+
+/**
+ *
+ * @author Fahim
+ */
+public class Polymorphism {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Animal a = new Animal();
+        a.makeNoise(); // print some sound
+    }
+    
+}
